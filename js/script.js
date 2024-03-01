12441976eb5a8c240e7624539ac807f8c285be5b
Trevor <tlist403@gmail.com>
2024-03-01 14:14:02 -0600

    Removes combat functions

--- js/script.js
+++ js/script.js
@@ -172,54 +172,6 @@
         monsterHealthText.innerText = monsterHealth;
     }
 
-    function attack() {
-        text.innerText = "The " + monsters[fighting].name + " attacks.";
-        text.innerText += " You attack it with your " + weapons[currentWeapon].name + ".";
-        health -= getMonsterAttackValue(monsters[fighting].level);
-        if (isMonsterHit()) {
-            monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1;
-        } else {
-            text.innerText += " You miss.";
-        }
-        healthText.innerText = health;
-        monsterHealthText.innerText = monsterHealth;
-        if (health <= 0) {
-            lose();
-        } else if (monsterHealth <= 0) {
-            if (fighting === 2) {
-                winGame();
-            } else {
-                defeatMonster();
-            }
-        }
-        if (Math.random() <= .1 && inventory.length !== 1) {
-            text.innerText += " Your " + inventory.pop() + " breaks.";
-            currentWeapon--;
-        }
-    }
-
-    function getMonsterAttackValue(level) {
-        const hit = (level * 5) - (Math.floor(Math.random() * xp));
-        console.log(hit);
-        return hit > 0 ? hit : 0;
-    }
-
-    function isMonsterHit() {
-        return Math.random() > .2 || health < 20;
-    }
-
-    function dodge() {
-        text.innerText = "You dodge the attack from the " + monsters[fighting].name;
-    }
-
-    function defeatMonster() {
-        gold += Math.floor(monsters[fighting].level * 6.7);
-        xp += monsters[fighting].level;
-        goldText.innerText = gold;
-        xpText.innerText = xp;
-        update(locations[4]);
-    }
-
     function lose() {
         update(locations[5]);
     }
@@ -238,40 +190,5 @@
         healthText.innerText = health;
         xpText.innerText = xp;
         goTown();
-    }
-
-    function easterEgg() {
-        update(locations[7]);
-    }
-
-    function pickTwo() {
-        pick(2);
-    }
-
-    function pickEight() {
-        pick(8);
-    }
-
-    function pick(guess) {
-        const numbers = [];
-        while (numbers.length < 10) {
-            numbers.push(Math.floor(Math.random() * 11));
-        }
-        text.innerText = "You picked " + guess + ". Here are the random numbers:\n";
-        for (let i = 0; i < 10; i++) {
-            text.innerText += numbers[i] + "\n";
-        }
-        if (numbers.includes(guess)) {
-            text.innerText += "Right! You win 20 gold!";
-            gold += 20;
-            goldText.innerText = gold;
-        } else {
-            text.innerText += "Wrong! You lose 10 health!";
-            health -= 10;
-            healthText.innerText = health;
-            if (health <= 0) {
-                lose();
-            }
-        }
     }
 })();
