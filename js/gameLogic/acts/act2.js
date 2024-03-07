66f1cbb849cb6a102ded50cf2cbaaf8b8fe56618
tlq68 <85347129+tlq68@users.noreply.github.com>
2024-03-07 10:20:13 -0600

    Distinguishes act2 for testing

--- js/gameLogic/acts/act2.js
+++ js/gameLogic/acts/act2.js
@@ -6,10 +6,10 @@ const act2 = (function() {
     
     // Array of objects containing the game choices
     const choices = [
-        {name: "start", id: 0,buttons: [{ text: "Go to store", func: () => handleChoice(0) },{ text: "Enter the house", func: () => handleChoice(1) },{ text: "Go to cave", func: () => handleChoice(2) },{ text: "Fight dragon", func: () => handleChoice(3) }],text: "You are in the town square. You see a sign that says Store."},
-        {name: "enter house",id: 1,buttons: [{ text: "Go back to store", func: () => handleChoice(0) },{ text: "Enter next room", func: () => handleChoice(1) },{ text: "Go to cave", func: () => handleChoice(2) },{ text: "Fight dragon", func: () => handleChoice(3) }],text: "You enter the house and are greeted."},
-        {name: "go cave",id: 2,buttons: [{ text: "Go back to the house", func: () => handleChoice(1) },{ text: "Go further in the cave", func: () => handleChoice(2) },{ text: "Fight dragon", func: () => handleChoice(3) }],text: "You are in a cave"},   
-        {name: "fighting dragon",id: 3,buttons: [{ text: "You are fighting a dragon", func: inFightWithDragon },{ text: "Give up", func: giveUp },{ text: "Just win", func: justWin }],text: "Behold the mighty dragon!!!"}
+        {name: "start", id: 0,buttons: [{ text: "act2Go to store", func: () => handleChoice(0) },{ text: "act2Enter the house", func: () => handleChoice(1) },{ text: "act2Go to cave", func: () => handleChoice(2) },{ text: "act2Fight dragon", func: () => handleChoice(3) }],text: "act2You are in the town square. You see a sign that says Store."},
+        {name: "enter house",id: 1,buttons: [{ text: "act2Go back to store", func: () => handleChoice(0) },{ text: "act2Enter next room", func: () => handleChoice(1) },{ text: "act2Go to cave", func: () => handleChoice(2) },{ text: "act2Fight dragon", func: () => handleChoice(3) }],text: "act2You enter the house and are greeted."},
+        {name: "go cave",id: 2,buttons: [{ text: "act2Go back to the house", func: () => handleChoice(1) },{ text: "act2Go further in the cave", func: () => handleChoice(2) },{ text: "act2Fight dragon", func: () => handleChoice(3) }],text: "act2You are in a cave"},   
+        {name: "fighting dragon",id: 3,buttons: [{ text: "act2You are fighting a dragon", func: inFightWithDragon },{ text: "act2Give up", func: giveUp },{ text: "act2Just win", func: justWin }],text: "act2Behold the mighty dragon!!!"}
     ];
 
     // Array of checkpoint objects
