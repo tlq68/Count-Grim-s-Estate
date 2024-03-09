beaa733a9479270be96dd46df07f2bfe3f78c140
tlq68 <85347129+tlq68@users.noreply.github.com>
2024-03-08 23:42:20 -0600

    Changes other text elements to working format

--- js/gameLogic/acts/act1.js
+++ js/gameLogic/acts/act1.js
@@ -27,11 +27,11 @@ const act1 = (function() {
     },
         
         {name: "enter house",id: 1,buttons: [{ text: "Go back to store", func: () => handleChoice(0) },{ text: "Enter next room", func: () => handleChoice(1) },{ text: "Go to cave", func: () => handleChoice(2) },{ text: "Fight dragon", func: () => handleChoice(3) }],
-        text: "You enter the house and are greeted."},
+        text: ["You enter the house and are greeted."]},
         {name: "go cave",id: 2,buttons: [{ text: "Go back to the house", func: () => handleChoice(1) },{ text: "Go further in the cave", func: () => handleChoice(2) },{ text: "Fight dragon", func: () => handleChoice(3) }],
-        text: "You are in a cave"},   
+        text: ["You are in a cave."]},   
         {name: "fighting dragon",id: 3,buttons: [{ text: "You are fighting a dragon", func: inFightWithDragon },{ text: "Give up", func: giveUp },{ text: "Just win", func: justWin }],
-        text: "Behold the mighty dragon!!!"}
+        text: ["Behold the mighty dragon!!!"]}
     ];
 
     // Array of checkpoint objects
